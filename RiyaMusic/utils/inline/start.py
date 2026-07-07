3888feac4fcbf376ac9b1d0218ef4a7e83fc2f93
nonsecularman <krishbharti3404@gmail.com>
2026-07-07 14:15:48 +0530

    Refactor inline button creation in start.py

--- RiyaMusic/utils/inline/start.py
+++ RiyaMusic/utils/inline/start.py
@@ -8,9 +8,13 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+            ),
         ],
     ]
     return buttons
@@ -24,16 +28,27 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                callback_data="settings_back_helper",
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID,
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL,
+            ),
         ],
-        [InlineKeyboardButton(text="⌯ sᴏᴜꝛᴄᴇ ⌯", url=f"https://github.com/TEAMRIYA/RiyaMusic")],
-    
+        [
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+            ),
+        ],
     ]
-    
     return buttons
